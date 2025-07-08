165ced6a0b74f084f54fa049d8817b45e20b7413
VKuzmin1974 <vkuzmin1974@gmail.com>
2025-07-08 12:06:10 +0300

    Команда старт

---
+++ fs.py
@@ -0,0 +1,12 @@
+from json import dumps, loads
+
+
+def save_json(db):
+    with open('db.json', 'w', encoding='UTF-8') as file:
+        file.write(dumps(db))
+
+
+def get_json():
+    with open('db.json', 'r', encoding='UTF-8') as file:
+        return loads(file.read())
+
--- main.py
+++ main.py
@@ -4,11 +4,28 @@ from aiogram import Bot, Dispatcher, types
 import random
 from os import getenv
 from dotenv import load_dotenv
+from aiogram.filters.command import Command
+from fs import get_json, save_json
 
 
 load_dotenv()
 bot = Bot(getenv('TOKEN'))
 dp = Dispatcher()
+db = get_json()
+
+@dp.message(Command('start'))
+async def start(message: types.message):
+    uid = str(message.from_user.id)
+    if uid in db:
+        await message.answer('Вы уже зарегистрированы')
+    else:
+        db[uid] = {'state': 'ожидание'}
+        save_json(db)
+        await message.answer('Добро пожаловать')
+
+
+
+
 
 async def main():
     await dp.start_polling(bot)

d7a95a3a5caf1e0cc063441c4448cd85ea1e1f7d
VKuzmin1974 <vkuzmin1974@gmail.com>
2025-08-10 13:00:43 +0300

    Создали функцию /stop

--- fs.py
+++ fs.py
@@ -3,7 +3,7 @@ from json import dumps, loads
 
 def save_json(db):
     with open('db.json', 'w', encoding='UTF-8') as file:
-        file.write(dumps(db))
+        file.write(dumps(db, indent=4))
 
 
 def get_json():
--- main.py
+++ main.py
@@ -36,16 +36,17 @@ async def create(message: types.message):
                 await message.answer('Не удалось создать игру. Попробуйте позже.')
                 return
         g_id = str(g_id)
-        db["games"][g_id] = {'move': 'крестики', 'table': ['.', '.', '.', '.', '.', '.', '.', '.', '.']}
+        db["games"][g_id] = {'move': 'крестики',
+                             'player1': uid,
+                             'player2': None,
+                            'table': ['.', '.', '.', '.', '.', '.', '.', '.', '.']}
         db["users"][uid]["state"] = 'играет'
         db["users"][uid]["game"] = g_id
         db["users"][uid]["point"] = 'крестики'
 
-        await message.answer(f'Вы создали игру, ее номер {g_id}. \n'
-                             f'Сейчас Ваш ход, выберите клетку от 1 до 9\n'
-                             f'1 2 3\n'
-                             f'4 5 6\n'
-                             f'7 8 9')
+
+        await message.answer(f"Вы создали игру, ее номер {g_id}. \n"
+                            f"{print_table(db['games'][g_id]['table'])}")
         save_json(db)
     else:
         await message.answer('Не удалось создать игру. Попробуйте позже.')
@@ -61,6 +62,7 @@ async def join(message: types.message):
                 db["users"][uid]["state"] = 'играет'
                 db["users"][uid]["game"] = arg
                 db["users"][uid]["point"] = 'нолики'
+                db["games"][arg]["player2"] = uid
                 save_json(db)
                 if db["games"][arg]['move'] == "крестики":
                     await message.answer(f'Вы подключились к игре, но сейчас не Ваш ход')
@@ -69,30 +71,35 @@ async def join(message: types.message):
             else:
                 await message.answer('Нет такой игры')
         else:
-            await message.answer('Введите /join номер игры')
+            await message.answer('Введите /join номер игры (например, /join 1234)')
     else:
         await message.answer('Не удалось подключиться')
+
+
+@dp.message(Command('stop'))
+async def stop(message: types.message):
+    uid = str(message.from_user.id)
+    if db["users"][uid]["state"] == 'играет':
+        g_id = db["users"][uid]["game"]
+        uid = db["games"][g_id]["player1"]
+        db["users"][uid]["state"] = 'ожидание'
+        p2 = db["games"][g_id].get("player2")
+        if p2:
+            db["users"][p2]["state"] = 'ожидание'
+            db["users"][p2]["game"] = None
+        del db["games"][g_id]
+        db["users"][uid]["game"] = None
+        save_json(db)
+
 
 def print_table(table):
     return (f'Сейчас Ваш ход, выберите клетку от 1 до 9\n'
             f'1 2 3\n'
             f'4 5 6\n'
             f'7 8 9\n\n'
-            f'{table[0]} {table[1]} {table[2]}'
-            f'{table[3]} {table[4]} {table[5]}'
-            f'{table[6]} {table[7]} {table[8]}')
-
-
-
-
-
-
-
-
-
-
-
-
+            f'{table[0]} {table[1]} {table[2]}\n'
+            f'{table[3]} {table[4]} {table[5]}\n'
+            f'{table[6]} {table[7]} {table[8]}\n')
 
 
 async def main():
